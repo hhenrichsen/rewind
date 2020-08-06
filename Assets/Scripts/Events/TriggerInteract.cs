168a90bbd1473fa291fc69ce1716d0e5d5fbebbb
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2020-08-06 02:32:43 -0600

    Add tag requirement for TriggerInteract.

--- Assets/Scripts/Events/TriggerInteract.cs
+++ Assets/Scripts/Events/TriggerInteract.cs
@@ -7,12 +7,21 @@ public class TriggerInteract : MonoBehaviour {
     private Collider collider;
     [SerializeField]
     public UnityEvent triggerEnter;
+    [SerializeField]
+    public string tag;
 
     public void Start() {
         this.collider = GetComponent<Collider>();
     }
 
     public void OnTriggerEnter(Collider other) {
+        if (tag != null && tag != "")
+        {
+            if (other.gameObject.tag != tag)
+            {
+                return;
+            }
+        }
         triggerEnter.Invoke();
     }
 }
